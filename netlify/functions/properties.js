c048bf8028bf18dcdd138ebfb2e357da50e9cb13
javiermendosalinas-afk <javiermendosalinas@gmail.com>
2026-05-04 17:26:22 -0600

    Update properties.js

--- netlify/functions/properties.js
+++ netlify/functions/properties.js
@@ -1,22 +1,43 @@
+const https = require('https');
+
 exports.handler = async (event) => {
   const API_KEY = 'hu5p841vw9zw5q50d0ye9dv97jlcwx';
   const page = event.queryStringParameters?.page || 1;
-  try {
-    const response = await fetch(
-      `https://api.easybroker.com/v1/properties?page=${page}&per_page=50`,
-      { headers: { 'X-Authorization': API_KEY, 'Accept': 'application/json' } }
-    );
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
+  
+  return new Promise((resolve) => {
+    const options = {
+      hostname: 'api.easybroker.com',
+      path: `/v1/properties?page=${page}&per_page=50`,
+      method: 'GET',
+      headers: {
+        'X-Authorization': API_KEY,
+        'Accept': 'application/json'
+      }
     };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ error: error.message })
-    };
-  }
+
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+          },
+          body: data
+        });
+      });
+    });
+
+    req.on('error', (error) => {
+      resolve({
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({ error: error.message })
+      });
+    });
+
+    req.end();
+  });
 };
